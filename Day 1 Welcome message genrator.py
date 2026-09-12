fd5cc7fbde7a768a32c13431fcf844805a62f7c8
skyestarli8ht <sachin.singh611051@gmail.com>
2026-09-13 01:30:26 +0530

    Day 1

---
+++ Day 1 Welcome message genrator.py
@@ -0,0 +1,12 @@
+#Welcome Message Genrator
+
+#take user info
+name = input("What's Your name? ")
+hobby = input("What's your favorite hobby? ")
+
+#Genrate a personalized welcome message
+print("\n***** Welcome *****")
+print(f"Hello, {name}!")
+print("Welcome to the world of python programming.")
+print(f"It's great to hear you love {hobby}.")
+print(f"Get Ready to build something new.")
