d65f76df78fe1497793b601c8d0b1f59f03be975
skyestarli8ht <sachin.singh611051@gmail.com>
2026-09-13 12:53:27 +0530

    Day 2

---
+++ Day 2 Personalized Greeting Program.py
@@ -0,0 +1,20 @@
+# Day 2 Personalized Greeting Program
+print("***** Personalized Greeting Program *****\n")
+name = str(input("What's Your name? "))
+age = int(input("What's Your Age? "))
+favouritColor = str(input("What's Your Favourite Coloe? "))
+
+print("\n---- Greeting ----")
+print(f"Hello, {name}!")
+print(f"You're {age} years old and {favouritColor} is a beautiful color.")
+print("You're now ready to start your python adventure 🚀")
+# *** What I learn ***
+# -> What is variable?
+# -> Data types: Strings, Integer, Floats, and boolean
+# -> Type Conversion:
+
+# # -> type() Give variable data type
+# -> int()
+# -> float()
+# -> str()
+# -> bool()
